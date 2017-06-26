2d8fef52f9e585edfaaefa6d55b62f9c976e122d
Elias Riedel Gårding <eliasrg@kth.se>
2017-06-26 11:46:23 -0700

    Implemented a Kalman filter for the observer/encoder

--- code/coding.py
+++ code/coding.py
@@ -6,3 +6,20 @@ class TrivialDecoder:
     def decode(self, *code):
         assert(len(code) == 1)
         return code[0]
+
+
+class KalmanFilter:
+    def __init__(self, estimate, variance):
+        self.estimate = estimate
+        self.variance = variance
+
+    def predict(self, alpha, W, u):
+        x_est, P = self.estimate, self.variance
+        self.estimate = alpha * x_est + u
+        self.variance = alpha**2 * P + W
+
+    def update(self, V, y):
+        x_est, P = self.estimate, self.variance
+        K = P / (P + V)
+        self.estimate = x_est + K * (y - x_est)
+        self.variance = K * V
