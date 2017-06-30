b6fddc1ab659219c0e3d32d62e1eec53f21b637c
Elias Riedel Gårding <eliasrg@kth.se>
2017-06-29 18:39:34 -0700

    Tried to replicate Fig 4 in the paper

--- code/test.py
+++ code/test.py
@@ -4,20 +4,38 @@ import matplotlib.pyplot as plt
 from simulation import Simulation, Parameters
 
 
-params = Parameters(
-        T = 100,
-        alpha = 2,
-        P1 = 1, # Same as W
-        W = 1, V = 1, SNR = 5, SDR0 = 1e100,
-        Q = 1, R = 1, F = 1)
+n_runs = 100
+T = 100
 
-globals().update(params.all()) # Bring parameters into scope
+LQG_average_trajectories = []
+for SNR in [2, 4]:
+    params = Parameters(
+            T = T,
+            alpha = 2,
+            P1 = 1, # Same as W
+            W = 1, V = 1, SNR = SNR, SDR0 = SNR,
+            Q = 1, R = 1, F = 1)
 
+    # globals().update(params.all()) # Bring parameters into scope
 
-sim = Simulation(params)
+    LQG_trajectories = []
+    for i in range(n_runs):
+        sim = Simulation(params)
+        LQG_trajectory = tuple(sim.LQG.evaluate(t)
+                               for t in sim.simulate(T + 1))
+        LQG_trajectories.append(LQG_trajectory)
 
+    LQG_slices = list(zip(*LQG_trajectories))
+    LQG_average_trajectory = np.array(list(map(np.mean, LQG_slices)))
+    LQG_average_trajectories.append(LQG_average_trajectory)
 
-LQG_trajectory = [sim.LQG.evaluate(t) for t in sim.simulate(T + 1)]
+for LQG_average_trajectory in LQG_average_trajectories:
+    # Plot in dB
+    plt.xlabel("Time [steps]")
+    plt.ylabel("Average LQR [dB]")
+    plt.plot(range(0, T + 1), 10 * np.log10(LQG_average_trajectory))
+    # plt.plot(range(0, T + 1), LQG_average_trajectory)
 
-plt.plot(range(0, T + 1), LQG_trajectory)
+plt.ylim(0, 50)
+plt.grid()
 plt.show()
