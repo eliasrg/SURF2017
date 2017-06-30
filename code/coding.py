6c58c8fc74a2f9ac616b6dbe37f5a8470fee36cf
Elias Riedel Gårding <eliasrg@kth.se>
2017-06-30 16:34:43 -0700

    Removed old Kalman filter implementation

--- code/coding.py
+++ code/coding.py
@@ -11,23 +11,6 @@ class TrivialDecoder:
         return -sim.params.alpha * x_est
 
 
-class KalmanFilter:
-    def __init__(self, estimate, variance):
-        self.estimate = estimate
-        self.variance = variance
-
-    def predict(self, alpha, W, u):
-        x_est, P = self.estimate, self.variance
-        self.estimate = alpha * x_est + u
-        self.variance = alpha**2 * P + W
-
-    def update(self, V, y):
-        x_est, P = self.estimate, self.variance
-        K = P / (P + V)
-        self.estimate = x_est + K * (y - x_est)
-        self.variance = K * V
-
-
 class Encoder:
     def __init__(self):
         # Initialize Kalman filter
