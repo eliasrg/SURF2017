9086c1f591bf2a4e22cc29fadf3a12782bd8b2c3
Elias Riedel Gårding <eliasrg@kth.se>
2017-06-30 16:13:16 -0700

    Refactored test.py to easier look for bugs

--- code/test.py
+++ code/test.py
@@ -1,20 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sys import setrecursionlimit
 
 from simulation import Simulation, Parameters
 
+setrecursionlimit(1 << 20)
 
-n_runs = 100
-T = 100
+
+n_runs = 1 << 8
+T = 1 << 10
 
 LQG_average_trajectories = []
-for SNR in [2, 4]:
+for SNR in [4]:
     params = Parameters(
             T = T,
             alpha = 2,
             P1 = 1, # Same as W
             W = 1, V = 1, SNR = SNR, SDR0 = SNR,
-            Q = 1, R = 1, F = 1)
+            Q = 1, R = 1, F = 1,
+            KC = 1, KS = 1)
 
     # globals().update(params.all()) # Bring parameters into scope
 
@@ -29,13 +33,17 @@ for SNR in [2, 4]:
     LQG_average_trajectory = np.array(list(map(np.mean, LQG_slices)))
     LQG_average_trajectories.append(LQG_average_trajectory)
 
-for LQG_average_trajectory in LQG_average_trajectories:
-    # Plot in dB
-    plt.xlabel("Time [steps]")
-    plt.ylabel("Average LQR [dB]")
-    plt.plot(range(0, T + 1), 10 * np.log10(LQG_average_trajectory))
-    # plt.plot(range(0, T + 1), LQG_average_trajectory)
+def plot():
+    for LQG_average_trajectory in LQG_average_trajectories:
+        # Plot in dB
+        plt.xlabel("Time [steps]")
+        plt.ylabel("Average LQR [dB]")
+        plt.plot(range(0, T + 1), 10 * np.log10(LQG_average_trajectory))
+        # plt.plot(range(0, T + 1), LQG_average_trajectory)
 
-plt.ylim(0, 50)
-plt.grid()
-plt.show()
+    plt.plot((0, T + 1),
+            10 * np.log10(params.LQR_inf_lower_bound()) * np.ones(2), 'r-')
+
+    plt.ylim(0, 50)
+    plt.grid()
+    plt.show(block=False)
