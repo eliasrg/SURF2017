24b525570919986ab4a7b079b97b4e201117140c
Elias Riedel Gårding <eliasrg@kth.se>
2017-07-05 21:31:45 -0700

    Added utilities.int_binsearch (translated from C++ contest code)

--- code/utilities.py
+++ code/utilities.py
@@ -9,3 +9,26 @@ def memoized(f):
         return table[args]
 
     return memo_f
+
+def int_binsearch(p, lo, hi):
+    """Takes a predicate p: int -> bool.
+    Assumes that there is some I (lo <= I < hi) such that
+    p(i) = (i >= I). Finds and returns I, or None if no such I exists."""
+    if lo == hi:
+        return None
+    elif lo + 1 == hi:
+        return lo if p(lo) else None
+    else:
+        mid = (lo + hi - 1) // 2
+        if p(mid): # mid >= I
+            return int_binsearch(p, lo, mid + 1)
+        else: # mid < I
+            return int_binsearch(p, mid + 1, hi)
+
+
+# Example: Search in list
+#
+# p = lambda i: return X[i]
+#
+# I = int_binsearch(p, 0, len(X));
+# x = X[I];
