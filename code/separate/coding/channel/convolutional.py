8a9f85ace5481304be8bf581724a9636b080f9ff
Elias Riedel Gårding <eliasrg@kth.se>
2017-07-31 16:16:50 -0700

    Refactored ConvolutionalCode for efficiency in the sequential decoding tree

--- code/separate/coding/channel/convolutional.py
+++ code/separate/coding/channel/convolutional.py
@@ -7,10 +7,17 @@ class ConvolutionalCode:
 
     def encode(self, data):
         """Performs one encoding step.
-    data is a list of column vectors in ℤ2^k."""
-        return sum(G @ b for G, b in zip(self.Gs, reversed(data))) % 2
+    data is a sequence of column vectors in ℤ2^k, in chronological order."""
+        return self.encode_reversed(reversed(list(data)))
+
+    def encode_reversed(self, reversed_data):
+        """Performs one encoding step.
+    data is a sequence of column vectors in ℤ2^k,
+    in reverse chronological order."""
+        return sum(G @ b for G, b in zip(self.Gs, reversed_data)) % 2
 
     def encode_sequence(self, data):
         """Encodes an entire sequence.
     data is a list of column vectors in ℤ2^k."""
+        data = list(data)
         return [self.encode(data[:i+1]) for i in range(len(data))]
