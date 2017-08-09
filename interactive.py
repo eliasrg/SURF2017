481e828cd2be08e65a996fefa8a5c05659da6a39
Elias Riedel Gårding <eliasrg@kth.se>
2017-08-09 11:17:38 -0700

    Moved interactive.py to toplevel

--- interactive.py
+++ interactive.py
@@ -1,3 +1,7 @@
+import sys, os
+sys.path.insert(0,
+        os.path.abspath(os.path.join(os.path.dirname(__file__), 'code')))
+
 from itertools import islice
 import numpy as np
 import scipy.stats as st
