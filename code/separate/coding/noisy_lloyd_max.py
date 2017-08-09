eliasrg/SURF2017

commit 3a99bc79f6841a4ea67f8e12d61fa215440634c6

Implemented the encoder, decoder and controller for noisy Lloyd-Max

code/separate/coding/noisy_lloyd_max.py
@@ -1,0 +1,67 @@
+from . import source
+from .convolutional import stack
+from utilities import to_column_vector
+
+class Encoder:
+    def __init__(self, sim, tracker, convolutional_code):
+        self.sim = sim
+        self.source_encoder = source.Encoder(sim, tracker)
+        self.convolutional_code = convolutional_code
+        self.bits_history = []
+
+    def encode(self, *msg):
+        # Quantize
+        i = self.source_encoder.encode(*msg)[i]
+
+        # Convert quantization index to bits
+        # TODO Worth doing in multiple ways in the digital case?
+        bits = int_to_bits(i)
+        self.bits_history.append(bits)
+
+        # Encode bits with tree code
+        codeword = self.convolutional_code.encode(self.bits_history)
+
+        # Send to channel
+        return bits
+
+class Decoder:
+    """Combined source and channel encoder."""
+    def __init__(self, sim, tracker, convolutional_code):
+        self.sim = sim
+        self.source_decoder = source.Decoder(sim, tracker)
+        self.source_decoder_history = [self.source_decoder]
+        self.convolutional_code = convolutional_code
+        self.received_codeword_history = []
+        self.stack_decoder = stack.StackDecoder(
+                self.convolutional_code, self.sim.channel.p) # Assume BSC
+
+    def decode(self, *msg):
+        received_codeword = to_column_vector(msg)
+        self.received_codeword_history.append(received_codeword)
+
+        # Decode using stack decoder
+        bits = self.stack_decoder.decode(self.received_codeword_history)
+
+        # Convert to quantization index
+        i = bits_to_int(bits)
+
+        # Recover quantized estimate of x
+        x_est = self.source_decoder.decode(i)
+        self.source_decoder_history.append(self.source_decoder.clone())
+
+        # TODO Check for mistake
+        mistake = None
+
+        return x_est, mistake
+
+
+def int_to_bits(i):
+    assert i in [0,1] # Assume k = 1 in line with 2-PAM
+    return to_column_vector([i])
+
+
+def bits_to_int(bits):
+    assert len(bits) == 1 # Assume k = 1 in line with 2-PAM
+    i = bits.flatten()[0]
+    assert i in [0,1]
+    return i

code/separate/control/noisy_lloyd_max.py
@@ -24,6 +24,28 @@ class Observer:
         return (x_est_ideal,)
 
 
+class Controller:
+    def __init__(self, sim):
+        self.sim = sim
+        self.u_history = []
+
+    def control(self, t, *msg):
+        sim = self.sim
+
+        # Decode the state estimate
+        assert len(msg) == 2 # One real number, one ??? to indicate mistake
+        x_est, mistake = msg
+
+        # Generate the control signal
+        u = -sim.params.L(t) * x_est
+
+        # Correct for mistake
+        if mistake:
+            pass # TODO
+
+        return u
+
+
 class DeterministicPlant:
     """The deterministic plant x(0) = 0; x(t+1) = α x(t) + u(t)."""
     def __init__(self, alpha):

commit 68790bfb1878631bec2ef7360a309c3464a9c9e0

Fixed programming errors introduced in 3a99bc79

code/separate/coding/noisy_lloyd_max.py
@@ -11,7 +11,7 @@ class Encoder:
 
     def encode(self, *msg):
         # Quantize
-        i = self.source_encoder.encode(*msg)[i]
+        i = self.source_encoder.encode(*msg)[0]
 
         # Convert quantization index to bits
         # TODO Worth doing in multiple ways in the digital case?
@@ -40,13 +40,13 @@ class Decoder:
         self.received_codeword_history.append(received_codeword)
 
         # Decode using stack decoder
-        bits = self.stack_decoder.decode(self.received_codeword_history)
+        bits = self.stack_decoder.decode_block(self.received_codeword_history)
 
         # Convert to quantization index
         i = bits_to_int(bits)
 
         # Recover quantized estimate of x
-        x_est = self.source_decoder.decode(i)
+        x_est = self.source_decoder.decode(i)[0]
         self.source_decoder_history.append(self.source_decoder.clone())
 
         # TODO Check for mistake