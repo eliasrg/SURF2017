eliasrg/SURF2017

commit b10c110f8a3f6beddf8227f066023ab87fd9d7dd

Wrote the observer for Lloyd-Max with noise

code/separate/control/noisy_lloyd_max.py
@@ -1,0 +1,34 @@
+class Observer:
+    """Calculates an estimate of the plant state as if the channel
+    were noiseless and transmits it to the controller."""
+    def __init__(self, sim):
+        self.sim = sim
+        self.x_u_ideal = DeterministicPlant(sim.alpha)
+        self.x_u_actual = DeterministicPlant(sim.alpha)
+
+    def observe(self, t, y):
+        # Update the controlled part of the actual plant
+        u_actual_previous = self.sim.globals.u[t-1] if t > 1 else 0
+        self.x_u_actual.step(u_actual_previous)
+
+        # Calculate the ideal (noiseless) estimate
+        x_est = y # TODO take observation noise into account
+        x_est_uncontrolled = x_est - self.x_u_actual.value
+        x_est_ideal = x_est_uncontrolled + self.x_u_ideal.value
+
+        # Update the controlled part of the ideal plant
+        u_ideal = -sim.params.L(t) * x_est_ideal
+        self.x_u_ideal.step(u_ideal)
+
+        # Pass to the source and channel encoders
+        return (x_est_ideal,)
+
+
+class DeterministicPlant:
+    """The deterministic plant x(0) = 0; x(t+1) = α x(t) + u(t)."""
+    def __init__(self, alpha):
+        self.value = 0
+        self.alpha = alpha
+
+    def step(self, u):
+        self.value = self.alpha * self.value + u

commit 8f4bca801c6f921e2577db43a57a3907c84ee8db

Corrected typos introduced in b10c110f

code/separate/control/noisy_lloyd_max.py
@@ -3,8 +3,8 @@ class Observer:
     were noiseless and transmits it to the controller."""
     def __init__(self, sim):
         self.sim = sim
-        self.x_u_ideal = DeterministicPlant(sim.alpha)
-        self.x_u_actual = DeterministicPlant(sim.alpha)
+        self.x_u_ideal = DeterministicPlant(sim.params.alpha)
+        self.x_u_actual = DeterministicPlant(sim.params.alpha)
 
     def observe(self, t, y):
         # Update the controlled part of the actual plant
@@ -17,7 +17,7 @@ class Observer:
         x_est_ideal = x_est_uncontrolled + self.x_u_ideal.value
 
         # Update the controlled part of the ideal plant
-        u_ideal = -sim.params.L(t) * x_est_ideal
+        u_ideal = -self.sim.params.L(t) * x_est_ideal
         self.x_u_ideal.step(u_ideal)
 
         # Pass to the source and channel encoders