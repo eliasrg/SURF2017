eliasrg/SURF2017

commit 39724f3ae40dfbc02f807801c4a379b64db2a4bb

Added argument stop_at to Node.{,reversed_}input_history

code/separate/coding/convolutional/node.py
@@ -26,14 +26,20 @@ class Node:
     def is_root(self):
         return self.parent is None
 
-    def reversed_input_history(self):
+    def reversed_input_history(self, stop_at=None):
+        """The reversed input history back to, but not including, the node
+        stop_at. If stop_at is None (the default), return the entire input
+        history."""
         node = self
-        while not node.is_root():
+        while not node.parent == stop_at:
             yield node.input_block
             node = node.parent
 
-    def input_history(self):
-        return reversed(list(self.reversed_input_history()))
+    def input_history(self, stop_at=None):
+        """The reversed input history back to, but not including, the node
+        stop_at. If stop_at is None (the default), return the entire input
+        history."""
+        return reversed(list(self.reversed_input_history(stop_at)))
 
     def extend(self, possible_input_blocks=None):
         """Creates and yields the children of this node.

commit 14d72b9e49c713a7a6a4652ab95ac77233d35bfd

Fixed a bug in Node.reversed_input_history when stop_at is not None
(introduced in 39724f3ae40dfbc02f807801c4a379b64db2a4bb)

code/separate/coding/convolutional/node.py
@@ -31,7 +31,7 @@ class Node:
         stop_at. If stop_at is None (the default), return the entire input
         history."""
         node = self
-        while not node.parent == stop_at:
+        while not (node.is_root() or node is stop_at):
             yield node.input_block
             node = node.parent
 