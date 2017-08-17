8ff6c4e539f3cfe2c1086f30db958700777e1c12
Elias Riedel Gårding <eliasrg@kth.se>
2017-08-17 14:05:32 -0700

    Made the stack decoder handle AWGN channels in addition to BSCs

--- code/separate/coding/convolutional/stack.py
+++ code/separate/coding/convolutional/stack.py
@@ -9,10 +9,15 @@ class StackDecoder:
     """Decodes a convolutional code transmitted over a BSC.
     Designed so that decoding incrementally by calling decode on a prefix
     of the received code sequence is efficient."""
-    def __init__(self, code, p, bias_mode='R'):
-        """Assumes binary symmetric channel with error probability p."""
+    def __init__(self, code, p=None, SNR=None, bias_mode='R'):
+        """If p is given, assumes BSC(p). If SNR is given, assumes AWGN(SNR)."""
         self.code = code
-        self.p = p
+        if p is not None:
+            self.p = p
+        elif SNR is not None:
+            self.SNR = SNR
+        else:
+            raise ValueError("p or SNR must be given")
 
         if bias_mode == 'R':
             self.bias = self.code.rate()
@@ -35,15 +40,31 @@ class StackDecoder:
     def extend(self, node, codeword):
         for child in node.extend():
             # Calculate the metric increment
-            p = self.p
             k = self.code.k
             n = self.code.n
             B = self.bias
-            d = hamming_distance(codeword, child.codeword)
 
-            metric_increment = \
-                    d * np.log2(p) + (n - d) * np.log2(1 - p) \
-                    + (1 - B) * n
+            if hasattr(self, 'p'):
+                # Binary codewords
+                assert all(z in [0,1] for z in codeword.flatten())
+                p = self.p
+                d = hamming_distance(codeword, child.codeword)
+                metric_increment = \
+                        d * np.log2(p) + (n - d) * np.log2(1 - p) \
+                        + (1 - B) * n
+            else:
+                # Real-valued codewords
+                assert all(isinstance(z, float) for z in codeword.flatten())
+                SNR = self.SNR
+
+                # log2( w(zi|ci) / p(zi) )
+                log_term = lambda z, c: \
+                    1 - SNR/2 * (z - (-1)**c)**2 \
+                    - np.log2(np.exp(-SNR/2 * (z - 1)**2)
+                            + np.exp(-SNR/2 * (z + 1)**2))
+
+                metric_increment = sum(log_term(z, c) - B
+                        for z, c in zip(codeword, child.codeword))
 
             child.metric = node.metric + metric_increment
 
